10d7aa83e4353486066ca532fedee13e65c8e3a1
David Jenkins <david.jenkins@eso.org>
2022-12-02 12:28:38 +0100

    read the docs fix docs logo fix

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -68,12 +68,12 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['../../system/centos-cfg/lark.png']
 
 modindex_common_prefix = ["lark."]
 
 html_theme_options = {
-    'logo': '../../system/centos-cfg/lark.png',
+    'logo': 'lark.png',
     'github_user': 'david-jenkins',
     'github_repo': 'lark',
     'fixed_sidebar': True,
